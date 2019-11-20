eec0997345020ba0710522d58cebbb9069ff1da4
Jared Grove <jaredkgrove@gmail.com>
2019-11-20 13:06:00 -0600

    imported line, recgtangle, and polyline images into lineTypeSelector

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -1,4 +1,4 @@
 class Circle < ApplicationRecord
-    has_one :element, as: :elementable
+    has_one :element, as: :elementable, dependent: :destroy
     accepts_nested_attributes_for :element
 end
--- client/src/components/lineTypeSelector.jsx
+++ client/src/components/lineTypeSelector.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import circleImage from '../images/Circle.PNG'
+import lineImage from '../images/Line.PNG'
+import rectangleImage from '../images/Rectangle.PNG'
+import polylineImage from '../images/polyLineImage.PNG'
  const LineTypeSelector = (props) => {
 
     const handleClick = (e) => {
@@ -13,9 +16,9 @@ import circleImage from '../images/Circle.PNG'
         <>
             
             <button className={isCurrentSetting('Circle') ? 'selected' : null} onClick={handleClick} value='Circle' style={{paddingTop:'21%', margin:'1%', background: `url(${circleImage})`, backgroundSize: 'cover'}}> </button>
-            <button className={isCurrentSetting('Line') ? 'selected' : null} onClick={handleClick} value='Line' style={{paddingTop:'21%', margin:'1%', background: "url(/images/Line.png)",  backgroundSize: 'cover'}}>  </button>
-            <button className={isCurrentSetting('Rectangle') ? 'selected' : null} onClick={handleClick} value='Rectangle' style={{paddingTop:'21%', margin:'1%', background: "url(/images/Rectangle.png)", backgroundSize: 'cover'}}> </button>
-            <button className={isCurrentSetting('Polyline') ? 'selected' : null} onClick={handleClick} value='Polyline' style={{paddingTop:'21%', margin:'1%', background: "url(/images/Free.png)", backgroundSize: 'cover'}}>  </button>
+            <button className={isCurrentSetting('Line') ? 'selected' : null} onClick={handleClick} value='Line' style={{paddingTop:'21%', margin:'1%', background: `url(${lineImage})`,  backgroundSize: 'cover'}}>  </button>
+            <button className={isCurrentSetting('Rectangle') ? 'selected' : null} onClick={handleClick} value='Rectangle' style={{paddingTop:'21%', margin:'1%', background: `url(${rectangleImage})`, backgroundSize: 'cover'}}> </button>
+            <button className={isCurrentSetting('Polyline') ? 'selected' : null} onClick={handleClick} value='Polyline' style={{paddingTop:'21%', margin:'1%', background: `url(${polylineImage})`, backgroundSize: 'cover'}}>  </button>
         </>
     )
  }
