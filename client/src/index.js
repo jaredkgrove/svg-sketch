048c68c927c32b42c0b44f9f36fc50892ef98c38
Jared Grove <jaredkgrove@gmail.com>
2019-11-19 20:43:26 -0600

    spelling

--- client/src/index.js
+++ client/src/index.js
@@ -8,9 +8,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 
 import thunk from 'redux-thunk';
-import sketchesReducer from './reducers/sketchesReducer.js'; 
-import currentSketchReducer from './reducers/currentSketchReducer.js'; 
-import sketchSettingsReducer from './reducers/sketchSettingsReducer.js'; 
+import sketchesReducer from './Reducers/sketchesReducer.js'; 
+import currentSketchReducer from './Reducers/currentSketchReducer.js'; 
+import sketchSettingsReducer from './Reducers/sketchSettingsReducer.js'; 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const rootReducer = combineReducers({sketches: sketchesReducer, currentSketch: currentSketchReducer, settings: sketchSettingsReducer})
