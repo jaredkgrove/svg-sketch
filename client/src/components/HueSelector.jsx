64c410580baa69ee24922fafa21a91b642e411dc
Jared Grove <jaredkgrove@gmail.com>
2019-11-25 18:35:49 -0600

    hueSelect allows for mouseMove selection instead of just mouseDown. Min y posiition is 0

--- client/src/components/HueSelector.jsx
+++ client/src/components/HueSelector.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
      constructor(){
         super()
         this.state = {
+            mouseDown: false,
             sliderPosition: 0,
             hue: 0
         }
@@ -33,19 +34,40 @@ import { connect } from 'react-redux';
       }
     
      componentDidUpdate(prevProps, prevState){
-         if(this.state.sliderPosition && this.state.sliderPosition !== prevState.sliderPosition ){
+         console.log(prevState)
+         if(this.state.sliderPosition && this.state.sliderPosition !== prevState.sliderPosition && (this.state.hue !== prevState.hue)){
             this.props.handleChange({h: this.state.hue})
          }
      }
        
     handleOnMouseDown = (e) => {
+        this.setState({
+            mouseDown: true
+        })
+        this.setSliderPosition(e)
+    } 
+
+    handleOnMouseMove = (e) => {
+        if(this.state.mouseDown){
+            this.setSliderPosition(e)
+        }
+    }
+
+    handleOnMouseUp = () => {
+        this.setState({
+            mouseDown: false
+        })
+    }
+
+    setSliderPosition = (e) => {
         this.hueBarClientRect = this.hueBar.current.getBoundingClientRect() 
         let y = e.clientY - this.hueBarClientRect.top
+        if(y < 0){y=0}
         this.setState({
             sliderPosition: y,
             hue: this.getHue(y)
         })
-    } 
+    }
 
     getHue = (pos) => (360 * (pos / this.hueBarClientRect.height))
 
@@ -53,7 +75,7 @@ import { connect } from 'react-redux';
 
     render(){
         return (
-            <div ref={this.hueBar} onMouseDown={this.handleOnMouseDown} className='hue-bar' style={{background: 'linear-gradient(to bottom,hsl(0,100%,50%),hsl(60,100%,50%),hsl(120,100%,50%),hsl(180,100%,50%),hsl(240,100%,50%),hsl(300,100%,50%),hsl(360,100%,50%))'}}>
+            <div ref={this.hueBar} onMouseDown={this.handleOnMouseDown} onMouseMove={this.handleOnMouseMove} onMouseUp={this.handleOnMouseUp}  className='hue-bar' style={{background: 'linear-gradient(to bottom,hsl(0,100%,50%),hsl(60,100%,50%),hsl(120,100%,50%),hsl(180,100%,50%),hsl(240,100%,50%),hsl(300,100%,50%),hsl(360,100%,50%))'}}>
                 <div style={{backgroundColor: `hsl(${this.state.hue},100%,50%)`, width: '140%', paddingTop:'70%', borderRadius:'35%', border:'1px solid white', position:'relative', top:`${this.state.sliderPosition-15}px`, display:'inline-block', right:'calc(20% + 1px)'}}></div>
             </div>
         )
